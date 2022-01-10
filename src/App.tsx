5cd7f5b6c11e3d2f109d8a10726cf2b176da4874
owen-mcmanus <56615953+owen-mcmanus@users.noreply.github.com>
2022-01-10 06:58:37 -0800

    fix service worker scope errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
 
-import Prototype from "./pages/prototype";
 import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
     <Dashboard />
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/955-scouting-pwa/proto" element={<Prototype />} />
-    //     <Route path="/955-scouting-pwa/" element={<Dashboard />} />
-    //   </Routes>
-    // </BrowserRouter>
   );
 }
 
