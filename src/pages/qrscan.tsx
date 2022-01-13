8184831b55b838012cc73ea9931b4fe80d50f55b
owen-mcmanus <owen.mcmanus727@gmail.com>
2022-01-12 21:19:18 -0800

    add qr reader (no functionality)

--- src/pages/qrscan.tsx
+++ src/pages/qrscan.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import "../styles/qrscan.css";
+
 import Nav from "../components/nav";
+import QrReader from "react-qr-scanner";
 
 export function QRScan() {
+  const [displayScanner, setDisplayScanner] = useState(false);
   return (
-    <div>
-      <h1>QR Scanner Page</h1>
+    <div id="qr-div">
+      <h2 id="qr-title">
+        Scan a QR Code to import match data from another device
+      </h2>
+      {displayScanner ? (
+        <QrReader
+          id="qr-reader"
+          delay={100}
+          onError={(e) => {
+            console.log(e);
+          }}
+          legacyMode={true}
+        />
+      ) : (
+        <div id="qr-nocamera" onClick={() => setDisplayScanner(true)}>
+          Click to start camera
+        </div>
+      )}
+
       <Nav selectedPage={0} />
     </div>
   );
