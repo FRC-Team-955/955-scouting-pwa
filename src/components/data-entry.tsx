49931855317d88c8e4cee30db741497071e0cf90
owen-mcmanus <owen.mcmanus727@gmail.com>
2022-01-30 14:34:43 -0800

    add dataEntry +/- buttons

--- src/components/data-entry.tsx
+++ src/components/data-entry.tsx
@@ -4,13 +4,18 @@ import { IEvent } from "../models";
 import "../styles/data-entry.css";
 
 interface dataEntryProps {
-  exit: Function
-  data: any
-  matchNumber: number
-  matchId: string
+  exit: Function;
+  data: any;
+  matchNumber: number;
+  matchId: string;
 }
 
-export default function DataEntry({ exit, matchNumber, matchId, data }:dataEntryProps) {
+export default function DataEntry({
+  exit,
+  matchNumber,
+  matchId,
+  data,
+}: dataEntryProps) {
   const [taxi, setTaxi] = useState(data?.taxi || false);
   const [autoLow, setAutoLow] = useState(data?.autoLow || 0);
   const [autoHigh, setAutoHigh] = useState(data?.autoHigh || 0);
@@ -18,9 +23,17 @@ export default function DataEntry({ exit, matchNumber, matchId, data }:dataEntry
   const [telopHigh, setTelopHigh] = useState(data?.telopHigh || 0);
   const [climb, setClimb] = useState(data?.climb || 0);
 
-  function saveData(e, forceSave=true){
-    e?.preventDefault()
-    if(forceSave || (taxi !== false || autoLow !== 0 || autoHigh !== 0 || telopLow !== 0 || telopHigh !== 0 || climb !== 0)){
+  function saveData(e, forceSave = true) {
+    e?.preventDefault();
+    if (
+      forceSave ||
+      taxi !== false ||
+      autoLow !== 0 ||
+      autoHigh !== 0 ||
+      telopLow !== 0 ||
+      telopHigh !== 0 ||
+      climb !== 0
+    ) {
       storeMatchData({
         id: `${matchId}-${data.teamNumber.toString()}`,
         teamNumber: data.teamNumber,
@@ -29,8 +42,8 @@ export default function DataEntry({ exit, matchNumber, matchId, data }:dataEntry
         autoHigh,
         telopLow,
         telopHigh,
-        climb
-      })
+        climb,
+      });
     }
   }
 
@@ -39,7 +52,13 @@ export default function DataEntry({ exit, matchNumber, matchId, data }:dataEntry
       <div className="card-body">
         <h3>
           Match {matchNumber}: {data.teamNumber}
-          <svg onClick={()=>{saveData(null, false); exit()}} viewBox="0 0 320 512">
+          <svg
+            onClick={() => {
+              saveData(null, false);
+              exit();
+            }}
+            viewBox="0 0 320 512"
+          >
             <path
               fill="currentColor"
               d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"
@@ -65,28 +84,51 @@ export default function DataEntry({ exit, matchNumber, matchId, data }:dataEntry
           </div>
           <div className="form-group">
             <label htmlFor="lowball-auto">balls in low: </label>
-            <input
-              className="form-inputs"
-              type="number"
-              placeholder="0"
-              id="lowball-auto"
-              value={autoLow}
-              onChange={(e) =>
-                setAutoLow(Math.max(parseInt(e.target.value), 0))
-              }
-            />
+            <div className="data-form-inputs" id="lowball-auto">
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setAutoLow(Math.max(autoLow - 1, 0));
+                }}
+              >
+                -
+              </button>
+              <span>{autoLow}</span>
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setAutoLow(autoLow + 1);
+                }}
+              >
+                +
+              </button>
+            </div>
           </div>
           <div className="form-group">
             <label htmlFor="highball-auto">balls in high: </label>
-            <input
-              className="form-inputs"
-              type="number"
-              placeholder="0"
-              value={autoHigh}
-              onChange={(e) =>
-                setAutoHigh(Math.max(parseInt(e.target.value), 0))
-              }
-            />
+            <div className="data-form-inputs" id="highball-auto">
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setAutoHigh(Math.max(autoHigh - 1, 0));
+                }}
+              >
+                -
+              </button>
+              <span>{autoHigh}</span>
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setAutoHigh(autoHigh + 1);
+                }}
+              >
+                +
+              </button>
+            </div>
           </div>
         </div>
 
@@ -94,46 +136,81 @@ export default function DataEntry({ exit, matchNumber, matchId, data }:dataEntry
           <h5>Telop</h5>
           <div className="form-group">
             <label htmlFor="lowball-telop">balls in low: </label>
-            <input
-              className="form-inputs"
-              type="number"
-              placeholder="0"
-              id="lowball-telop"
-              value={telopLow}
-              onChange={(e) =>
-                setTelopLow(Math.max(parseInt(e.target.value), 0))
-              }
-            />
+            <div className="data-form-inputs" id="lowball-telop">
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setTelopLow(Math.max(telopLow - 1, 0));
+                }}
+              >
+                -
+              </button>
+              <span>{telopLow}</span>
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setTelopLow(telopLow + 1);
+                }}
+              >
+                +
+              </button>
+            </div>
           </div>
 
           <div className="form-group">
             <label htmlFor="highball-telop">balls in high: </label>
-            <input
-              className="form-inputs"
-              type="number"
-              placeholder="0"
-              id="highball-telop"
-              value={telopHigh}
-              onChange={(e) =>
-                setTelopHigh(Math.max(parseInt(e.target.value), 0))
-              }
-            />
+            <div className="data-form-inputs" id="highball-telop">
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setTelopHigh(Math.max(telopHigh - 1, 0));
+                }}
+              >
+                -
+              </button>
+              <span>{telopHigh}</span>
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setTelopHigh(telopHigh + 1);
+                }}
+              >
+                +
+              </button>
+            </div>
           </div>
           <div className="form-group">
             <label htmlFor="climb-telop">level of climb: </label>
-            <input
-              className="form-inputs"
-              type="number"
-              placeholder="(0-4)"
-              id="climb-telop"
-              value={climb}
-              onChange={(e) =>
-                setClimb(Math.min(4, Math.max(parseInt(e.target.value), 0)))
-              }
-            />
+            <div className="data-form-inputs" id="climb-telop">
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setClimb(Math.max(climb - 1, 0));
+                }}
+              >
+                -
+              </button>
+              <span>{climb}</span>
+              <button
+                className="btn btn-primary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setClimb(Math.min(climb + 1, 4));
+                }}
+              >
+                +
+              </button>
+            </div>
           </div>
         </div>
-        <button className="btn btn-primary" onClick={saveData}>Save Data</button>
+        <button className="btn btn-primary" onClick={saveData}>
+          Save Data
+        </button>
       </form>
     </div>
   );
