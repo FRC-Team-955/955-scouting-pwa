49cd3a616407be06dac998077c6e360146aeb501
owen-mcmanus <owen.mcmanus727@gmail.com>
2022-01-30 16:15:00 -0800

    moved csv viewer

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -68,22 +68,49 @@ export default function Dashboard() {
 
   return (
     <div>
-
       {showQRgen ? (
         <QRgen exit={() => setShowQRgen(false)} data={QRdata} />
       ) : (
         <></>
       )}
+      <div
+        style={{
+          margin: "0 1rem",
+          marginTop: "1.5rem",
+        }}
+      >
+        <select value={week} id="week-select" onChange={handleChange}>
+          <option value={1}>Week 1</option>
+          <option value={2}>Week 2</option>
+          <option value={3}>Week 3</option>
+          <option value={4}>Week 4</option>
+          <option value={5}>Week 5</option>
+          <option value={6}>Week 6</option>
+        </select>
 
-      <select value={week} id="week-select" onChange={handleChange}>
-        <option value={1}>Week 1</option>
-        <option value={2}>Week 2</option>
-        <option value={3}>Week 3</option>
-        <option value={4}>Week 4</option>
-        <option value={5}>Week 5</option>
-        <option value={6}>Week 6</option>
-      </select>
-
+        {showCsvViewer ? (
+          <CsvViewer exit={() => setShowCsvViewer(false)} eventId={eventKey} />
+        ) : (
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="cloud-arrow-down"
+            className="svg-inline--fa fa-cloud-arrow-down"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 640 512"
+            width="2rem"
+            onClick={() => setShowCsvViewer(true)}
+            style={{ display: "inline", float: "right" }}
+          >
+            <path
+              fill="currentColor"
+              d="M537.6 226.6C541.7 215.9 544 204.2 544 192c0-53-43-96-96-96c-19.69 0-38.09 6-53.31 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.703 .1094 5.445 .2031 8.133C40.2 219.8 0 273.2 0 336C0 415.5 64.5 480 144 480H512c70.69 0 128-57.3 128-128C640 290.1 596 238.4 537.6 226.6zM424.1 320.1l-88 88C334.4 411.5 328.5 416 320 416s-14.4-4.467-16.97-7.031l-88-88c-9.375-9.375-9.375-24.56 0-33.94s24.56-9.375 33.94 0L296 334.1V184C296 170.8 306.8 160 320 160s24 10.75 24 24v150.1l47.03-47.03c9.375-9.375 24.56-9.375 33.94 0S434.3 311.6 424.1 320.1z"
+            ></path>
+          </svg>
+        )}
+      </div>
       <select
         value={eventKey}
         name="Event"
@@ -102,17 +129,6 @@ export default function Dashboard() {
       </select>
 
       <hr />
-
-      {showCsvViewer ? (
-        <CsvViewer exit={() => setShowCsvViewer(false)} eventId={eventKey}/>
-      ) : (
-        <button
-          className="btn btn-primary"
-          onClick={() => setShowCsvViewer(true)}
-        >
-          Download CSV
-        </button>
-      )}
 
       <div className="matches">
         <p>Matches</p>
