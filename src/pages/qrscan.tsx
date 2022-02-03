5d5f6022176e3ed4a1b54ff044821ad431162de0
owen-mcmanus <owen.mcmanus727@gmail.com>
2022-02-03 13:29:32 -0800

    uploads data to firebase on qr scan

--- src/pages/qrscan.tsx
+++ src/pages/qrscan.tsx
@@ -4,6 +4,7 @@ import "../styles/qrscan.css";
 import Nav from "../components/nav";
 import { Html5QrcodeScanner } from "html5-qrcode"; //https://www.npmjs.com/package/html5-qrcode
 import { storeMatchData } from "../api/local-storage";
+import { sendMatchDataFirebase } from "../api/firebase-api";
 
 export function QRScan() {
   let html5QrcodeScanner;
@@ -17,11 +18,21 @@ export function QRScan() {
     html5QrcodeScanner.render(onScanSuccess, onScanFailure);
   }, []);
 
-  function onScanSuccess(decodedText) {
+  async function onScanSuccess(decodedText) {
     // function runs after sucesfull scan of qr code
     const dataArr = decodedText.split(","); // qr gives a comma seperated string that is split into an array
     if (dataArr.length >= 8) {
-      storeMatchData({
+      await storeMatchData({
+        id: dataArr[0],
+        teamNumber: parseInt(dataArr[1]),
+        taxi: dataArr[2] === "true",
+        autoLow: parseInt(dataArr[3]),
+        autoHigh: parseInt(dataArr[4]),
+        telopLow: parseInt(dataArr[5]),
+        telopHigh: parseInt(dataArr[6]),
+        climb: parseInt(dataArr[7]),
+      });
+      await sendMatchDataFirebase({
         id: dataArr[0],
         teamNumber: parseInt(dataArr[1]),
         taxi: dataArr[2] === "true",
