8d6990a4138dfa40b2eaff3fca22150657cc6e2b
owen-mcmanus <owen.mcmanus727@gmail.com>
2022-03-18 22:59:20 -0700

    add offline page qr gen

--- src/pages/offline.tsx
+++ src/pages/offline.tsx
@@ -1,239 +1,322 @@
 import { useState } from "react";
 import { storeMatchData } from "../api/local-storage";
 import Nav from "../components/nav";
+import QRCode from "qrcode";
 
-function Offline(){
-    const [taxi, setTaxi] = useState(false);
-    const [autoLow, setAutoLow] = useState(0);
-    const [autoHigh, setAutoHigh] = useState(0);
-    const [telopLow, setTelopLow] = useState(0);
-    const [telopHigh, setTelopHigh] = useState(0);
-    const [defense, setDefense] = useState(0);
-    const [climb, setClimb] = useState(0);
-    const [notes, setNotes] = useState("");
-    const [teamNumber, setTeamNumber] = useState<number>(0);
-    const [matchNumber, setMatchNumber] = useState<number>(0);
-    const [color, setColor] = useState<number>(0);
-    const [dsNumber, setDsNumber] = useState<number>(0);
+function Offline() {
+  const [taxi, setTaxi] = useState(false);
+  const [autoLow, setAutoLow] = useState(0);
+  const [autoHigh, setAutoHigh] = useState(0);
+  const [telopLow, setTelopLow] = useState(0);
+  const [telopHigh, setTelopHigh] = useState(0);
+  const [defense, setDefense] = useState(0);
+  const [climb, setClimb] = useState(0);
+  const [notes, setNotes] = useState("");
+  const [teamNumber, setTeamNumber] = useState<number>(0);
+  const [matchNumber, setMatchNumber] = useState<number>(0);
+  const [color, setColor] = useState<number>(0);
+  const [dsNumber, setDsNumber] = useState<number>(0);
+  const [showQR, setShowQR] = useState(false);
 
-    function storeData() {
-        storeMatchData({
-          id: `${'2022caph_qm'+matchNumber.toString()}-${teamNumber.toString()}*${(matchNumber-1) * 6 + 0}`,
-          teamNumber: teamNumber,
-          taxi,
-          autoLow,
-          autoHigh,
-          telopLow,
-          telopHigh,
-          defense,
-          climb,
-          notes,
-        });
-      }
+  const [qr, setQr] = useState("");
+  async function generateQR(text) {
+    try {
+      setQr(await QRCode.toDataURL(text)); // generates qr image and returns url of image
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-    return(
+  function storeData() {
+    const id = `${
+      "2022caph_qm" + matchNumber.toString()
+    }-${teamNumber.toString()}*${(matchNumber - 1) * 6 + color + dsNumber}`;
+    generateQR(
+      `${id},${teamNumber},${taxi},${autoLow},${autoHigh},${telopLow},${telopHigh},${defense},${climb}, ${notes}`
+    );
+    setShowQR(true);
+    storeMatchData({
+      id,
+      teamNumber: teamNumber,
+      taxi,
+      autoLow,
+      autoHigh,
+      telopLow,
+      telopHigh,
+      defense,
+      climb,
+      notes,
+    });
+  }
+
+  return (
     <div>
-        <div>
+      <div>
         <h3 className="card-body">
-        <div className="form-group">
+          <div className="form-group">
             <label htmlFor="highball-telop">Team Number: </label>
             <div className="data-form-inputs" id="highball-telop">
-              <input type="text" value={teamNumber} onChange={(e)=>setTeamNumber(parseInt(e.target.value)||0)}></input>
+              <input
+                type="text"
+                value={teamNumber}
+                onChange={(e) => setTeamNumber(parseInt(e.target.value) || 0)}
+              ></input>
             </div>
           </div>
           <div className="form-group">
             <label htmlFor="highball-telop">Match Number: </label>
             <div className="data-form-inputs" id="highball-telop">
-              <input type="text" value={matchNumber} onChange={(e)=>setMatchNumber(parseInt(e.target.value)||0)}></input>
+              <input
+                type="text"
+                value={matchNumber}
+                onChange={(e) => setMatchNumber(parseInt(e.target.value) || 0)}
+              ></input>
             </div>
           </div>
-      </h3>
-      <form>
-        <div className="data-group">
-          <h5>Auto</h5>
-          <div className="form-check">
+          <div>
             <input
-              className="form-check-input"
-              type="checkbox"
-              placeholder="taxi"
-              id="taxi-check"
-              defaultChecked={taxi}
-              checked={taxi}
-              onChange={(e) => setTaxi(e.target.checked)}
+              type="radio"
+              onClick={() => setColor(0)}
+              checked={color === 0}
+            />{" "}
+            Red
+            <input
+              type="radio"
+              onClick={() => setColor(3)}
+              checked={color === 3}
+            />{" "}
+            Blue
+          </div>
+          <div>
+            <input
+              type="radio"
+              onClick={() => setDsNumber(0)}
+              checked={dsNumber === 0}
             />
-            <label className="form-check-label" htmlFor="taxi-check">
-              Taxi
-            </label>
+            1
+            <input
+              type="radio"
+              onClick={() => setDsNumber(1)}
+              checked={dsNumber === 1}
+            />
+            2
+            <input
+              type="radio"
+              onClick={() => setDsNumber(2)}
+              checked={dsNumber === 2}
+            />
+            3
           </div>
-          <div className="form-group">
-            <label htmlFor="lowball-auto">balls in low: </label>
-            <div className="data-form-inputs" id="lowball-auto">
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setAutoLow(Math.max(autoLow - 1, 0));
-                }}
-              >
-                -
-              </button>
-              <span>{autoLow}</span>
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setAutoLow(autoLow + 1);
-                }}
-              >
-                +
-              </button>
-            </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="highball-auto">balls in high: </label>
-            <div className="data-form-inputs" id="highball-auto">
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setAutoHigh(Math.max(autoHigh - 1, 0));
-                }}
-              >
-                -
-              </button>
-              <span>{autoHigh}</span>
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setAutoHigh(autoHigh + 1);
-                }}
-              >
-                +
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className="data-group">
-          <h5>Telop</h5>
-          <div className="form-group">
-            <label htmlFor="lowball-telop">balls in low: </label>
-            <div className="data-form-inputs" id="lowball-telop">
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setTelopLow(Math.max(telopLow - 1, 0));
-                }}
-              >
-                -
-              </button>
-              <span>{telopLow}</span>
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setTelopLow(telopLow + 1);
-                }}
-              >
-                +
-              </button>
-            </div>
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="highball-telop">balls in high: </label>
-            <div className="data-form-inputs" id="highball-telop">
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setTelopHigh(Math.max(telopHigh - 1, 0));
-                }}
-              >
-                -
-              </button>
-              <span>{telopHigh}</span>
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setTelopHigh(telopHigh + 1);
-                }}
-              >
-                +
-              </button>
-            </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="defense-telop">defense (0-2): </label>
-            <div className="data-form-inputs" id="defense-telop">
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setDefense(Math.max(defense - 1, 0));
-                }}
-              >
-                -
-              </button>
-              <span>{defense}</span>
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setDefense(Math.min(defense + 1, 2));
-                }}
-              >
-                +
-              </button>
-            </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="climb-telop">level of climb: </label>
-            <div className="data-form-inputs" id="climb-telop">
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setClimb(Math.max(climb - 1, 0));
-                }}
-              >
-                -
-              </button>
-              <span>{climb}</span>
-              <button
-                className="btn btn-primary"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setClimb(Math.min(climb + 1, 4));
-                }}
-              >
-                +
-              </button>
-            </div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="notes">notes (130 chars): </label>
-            <br />
-            <div className="data-form-inputs" id="notes">
-              <textarea
-                value={notes}
-                onChange={(e) => setNotes(e.target.value.replaceAll(",", ";"))}
-                maxLength={130}
-                rows={4}
-                cols={35}
+        </h3>
+        <form>
+          <div className="data-group">
+            <h5>Auto</h5>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                placeholder="taxi"
+                id="taxi-check"
+                defaultChecked={taxi}
+                checked={taxi}
+                onChange={(e) => setTaxi(e.target.checked)}
               />
+              <label className="form-check-label" htmlFor="taxi-check">
+                Taxi
+              </label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="lowball-auto">balls in low: </label>
+              <div className="data-form-inputs" id="lowball-auto">
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setAutoLow(Math.max(autoLow - 1, 0));
+                  }}
+                >
+                  -
+                </button>
+                <span>{autoLow}</span>
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setAutoLow(autoLow + 1);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="highball-auto">balls in high: </label>
+              <div className="data-form-inputs" id="highball-auto">
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setAutoHigh(Math.max(autoHigh - 1, 0));
+                  }}
+                >
+                  -
+                </button>
+                <span>{autoHigh}</span>
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setAutoHigh(autoHigh + 1);
+                  }}
+                >
+                  +
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
-      <button className="btn btn-primary" onClick={storeData}>Save Data</button>
-        </div>
-        <Nav selectedPage={2}/>
+
+          <div className="data-group">
+            <h5>Telop</h5>
+            <div className="form-group">
+              <label htmlFor="lowball-telop">balls in low: </label>
+              <div className="data-form-inputs" id="lowball-telop">
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTelopLow(Math.max(telopLow - 1, 0));
+                  }}
+                >
+                  -
+                </button>
+                <span>{telopLow}</span>
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTelopLow(telopLow + 1);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="highball-telop">balls in high: </label>
+              <div className="data-form-inputs" id="highball-telop">
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTelopHigh(Math.max(telopHigh - 1, 0));
+                  }}
+                >
+                  -
+                </button>
+                <span>{telopHigh}</span>
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setTelopHigh(telopHigh + 1);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="defense-telop">defense (0-2): </label>
+              <div className="data-form-inputs" id="defense-telop">
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setDefense(Math.max(defense - 1, 0));
+                  }}
+                >
+                  -
+                </button>
+                <span>{defense}</span>
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setDefense(Math.min(defense + 1, 2));
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="climb-telop">level of climb: </label>
+              <div className="data-form-inputs" id="climb-telop">
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setClimb(Math.max(climb - 1, 0));
+                  }}
+                >
+                  -
+                </button>
+                <span>{climb}</span>
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setClimb(Math.min(climb + 1, 4));
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="notes">notes (130 chars): </label>
+              <br />
+              <div className="data-form-inputs" id="notes">
+                <textarea
+                  value={notes}
+                  onChange={(e) =>
+                    setNotes(e.target.value.replaceAll(",", ";"))
+                  }
+                  maxLength={130}
+                  rows={4}
+                  cols={35}
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+        <button className="btn btn-primary" onClick={storeData}>
+          Show QR
+        </button>
+        {showQR ? (
+          <div
+            className="qrcard card"
+            style={{ width: "16rem", height: "20rem" }}
+          >
+            <div className="card-body">
+              <svg onClick={() => setShowQR(false)} viewBox="0 0 320 512">
+                <path
+                  fill="currentColor"
+                  d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"
+                ></path>
+              </svg>
+              <h4>Team: {teamNumber}</h4>
+              <img src={qr} alt=""></img>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+      <Nav selectedPage={2} />
     </div>
-    )
+  );
 }
 
 export default Offline;
